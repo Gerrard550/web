6d76df731bd30fa2bc5976133b6618a0d099a341
Gerrard550 <privatesystem07@gmail.com>
2024-08-27 23:25:44 -0700

    using js on slides

---
+++ script.js
@@ -0,0 +1,32 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    SpaceBetween: 30
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+           slidesPerview: 1
+        },
+
+        768: {
+            slidesPerview: 2
+         },
+
+         1024: {
+            slidesPerview: 3
+
+         }                     
+    }
+});
